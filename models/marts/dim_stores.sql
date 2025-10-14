08129528fe62b7a77ff419002203019901c98276
zach cooper <zach.cooper@dbtlabsworkshop.com>
2025-10-14 19:22:22 +0000

    dim_stores created

---
+++ models/marts/dim_stores.sql
@@ -0,0 +1,38 @@
+with src as (
+    select *
+    from {{ref('stg_jaffle_shop__stores')}}
+)
+
+, customers as (
+    select *
+    from {{ref('dim_customers')}}
+    )
+
+, orders as (
+    select *
+    from {{ref('stg_jaffle_shop__orders')}}
+    )
+
+, customer_orders as (
+    select c.customer_name
+        , o.*
+        
+    from customers c 
+    join orders o on o.customer_id = c.customer_id
+)
+
+, most_pop_customer as (
+    select store_id
+        , count(order_id) as order_cnt
+        , dense_rank() over (partition by store_id order by order_cnt desc) as order_rank
+    from customer_orders
+    group by store_id
+)
+
+select 
+    src.store_id
+    , opened_at as open_dt
+    , store_location as location
+    , tax_rate
+from src
+join most_pop_customer m on m.store_id = src.store_id and order_rank = 1
